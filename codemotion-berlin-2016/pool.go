8b67a0fa97d3f5198f6b5341d517afb918c5d9dc
Aaron Schlesinger <arschles@gmail.com>
2016-10-24 13:59:20 +0200

    more comments

--- codemotion-berlin-2016/pool.go
+++ codemotion-berlin-2016/pool.go
@@ -47,7 +47,8 @@ func main() {
 	}
 
 	// receive work from all the workers. results will receive as they are
-	// completed by workers
+	// completed by workers. use the sync.WaitGroup as a simple barrier to wait
+	// until all receies to complete
 	var wg sync.WaitGroup
 	for _, wi := range workItems {
 		wg.Add(1)
