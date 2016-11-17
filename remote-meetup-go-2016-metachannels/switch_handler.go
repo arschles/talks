7e65a36c67cd7b88e8d8a05a55d86d289b977721
Aaron Schlesinger <arschles@gmail.com>
2016-11-17 15:50:16 -0800

    Update switch_handler.go

--- remote-meetup-go-2016-metachannels/switch_handler.go
+++ remote-meetup-go-2016-metachannels/switch_handler.go
@@ -5,6 +5,8 @@ import (
 )
 
 func switchHandler(w http.ResponseWriter, r *http.Request) {
+	// NOTE: this isn't concurrency safe. In production code, use a mutex or manage these channels
+	// inside a single goroutine that acts as a multiplexer (kinda like an actor)
 	tmpCh := jsonCh
 	jsonCh = base64Ch
 	base64Ch = tmpCh
